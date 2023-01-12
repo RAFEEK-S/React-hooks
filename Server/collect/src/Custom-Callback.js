a1304dc63afb78f416c4dbe86cb15a621c292d0c
RAFEEK-S <rafeeksarthar73@gmail.com>
2023-01-12 19:34:18 +0530

    custom callback

---
+++ Server/collect/src/Custom-Callback.js
@@ -0,0 +1,57 @@
+
+// const product = getProduct(1);
+// console.log(product);
+
+getProduct(1, function (product) {
+    console.log('Product', product);
+    getListofMobiles(product.productName, (items) => {
+      console.log('Mobiles', items);
+      getItems(items[3], function (items) {
+        console.log('Items', items);
+      })
+    })
+  })
+  
+  function getProduct(id, callback) {
+    setTimeout(() => {
+      console.log('Getting a product from the available list...')
+      callback({ id: id, productName: 'Mobile' })
+      // return { id: id, productName: 'Mobile phones' }
+    }, 2000);
+  }
+  
+  
+  function getListofMobiles(item, callback) {
+    setTimeout(() => {
+      console.log('Getting mobiles from the available list...')
+      callback(['Apple', 'Samsung', 'MI', 'Oppo', 'OnePlus'])
+    }, 2000);
+  }
+  
+  function getItems(item, callback) {
+    if (item == 'Apple') {
+      setTimeout(() => {
+        console.log('Items list.....')
+        callback(['MAC', 'IPHONE', 'AirPods', 'IWatch', 'Tab'])
+      }, 2000);
+    }
+    if (item == 'Samsung') {
+      setTimeout(() => {
+        console.log('Items list.....')
+        callback(['Flip', 'Fold', 'Watch', 'Tab'])
+      }, 2000);
+    }
+    if (item == 'MI') {
+        setTimeout(() => {
+          console.log('Items list.....')
+          callback(['redmix', 'ipadx', 'redmiwatch', 'redmiTab'])
+        }, 2000);
+      }
+      if (item == 'Oppo') {
+        setTimeout(() => {
+          console.log('Items list.....')
+          callback(['oppomobile', 'oppoipad', 'oppowatch', 'oppoTab'])
+        }, 2000);
+      }
+  }
+  
